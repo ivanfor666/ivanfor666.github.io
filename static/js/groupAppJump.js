31e33ed74cdabc763f7bd829d4803e4e7adbd07c
ivan <you@example.comuser.email>
2021-04-18 18:06:14 +0800

    v2.1  跳转appGroup,antd

---
+++ static/js/groupAppJump.js
@@ -0,0 +1,19 @@
+!(function f() {
+    var odiv = document.createElement('div');
+    odiv.setAttribute("class", "right_top");
+    odiv.innerText = "to";
+    odiv.style = `background: #1a59b7;color:#ffffff;overflow: hidden;z-index: 9999;position: fixed;line-height: 30px;text-align:center;width: 30px;height: 30px;border-bottom-left-radius: 4px;border-bottom-right-radius: 4px;border-top-left-radius: 4px;border-top-right-radius: 4px;right: 10px;top: 10px;border-radius: 20px;`
+    document.body.append(odiv);
+    odiv.addEventListener('click', function () {
+        var otitle = document.getElementsByTagName("title")[0];
+        var dataset = otitle.dataset;
+        if (typeof (dataset) === "object") {
+            if (dataset.apptype === "1") {
+                window.location.href = window.location.href + "build"
+            } else {
+                console.log(window.location)
+                window.location.href = window.location.origin;
+            }
+        }
+    })
+}())
