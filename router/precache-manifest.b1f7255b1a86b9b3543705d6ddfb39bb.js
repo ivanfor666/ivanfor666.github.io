2a73f43af0e28cbe509cbbd7257af2ff04676f7b
ivan <you@example.comuser.email>
2021-04-24 15:57:29 +0800

    router app

---
+++ router/precache-manifest.b1f7255b1a86b9b3543705d6ddfb39bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f45eda6dcbdde2a7075c9298ffa9ab63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46a9f3da14f5dd5161a6",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dde7063934c7cd78fc86",
+    "url": "/static/js/2.766dcb2e.chunk.js"
+  },
+  {
+    "revision": "46a9f3da14f5dd5161a6",
+    "url": "/static/js/main.6621d0eb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
