29c6a11508a142a92711e45ba3bc13da595700e2
ivan <you@example.comuser.email>
2021-04-27 22:09:55 +0800

    new app  react-router-antd

--- static/js/groupAppJump.js
+++ static/js/groupAppJump.js
@@ -9,7 +9,7 @@
         var dataset = otitle.dataset;
         if (typeof (dataset) === "object") {
             if (dataset.apptype === "1") {
-                window.open(window.location.href + "router-demo")
+                window.open("router-demo")
 
             } else {
                 console.log(window.location)
